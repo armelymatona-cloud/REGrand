68645678eb25ee7a85d283d57446c616940bd935
armelymatona-cloud <armelymatona@gmail.com>
2026-06-27 14:17:14 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+import os
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-# IDs Telegram des 3 utilisateurs autorisés
-AUTHORIZED_USERS = [8045306923, 987654321, 555555555]
+
+# Lecture de la variable, on sépare les IDs par une virgule dans Railway
+auth_users_str = os.getenv("AUTHORIZED_USERS", "")
+AUTHORIZED_USERS = [int(user_id.strip()) for user_id in auth_users_str.split(",") if user_id.strip()]
