9a1afd3ad35f320ad4f4cc6fe671818ffbdb0f9a
armelymatona-cloud <armelymatona@gmail.com>
2026-06-27 14:11:45 +0200

    Use environment variable for BOT_TOKEN
    Replaced hardcoded BOT_TOKEN with environment variable.

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
-# config.py
-BOT_TOKEN = "8708628597:AAEVVIO6c5wAwMRAzCl_PGttklI5lrZlV1Q"
-
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 # IDs Telegram des 3 utilisateurs autorisés
 AUTHORIZED_USERS = [8045306923, 987654321, 555555555]
