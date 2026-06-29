3e8a8791aac5f88b2a7528253ad4befd05caa0f1
armelymatona-cloud <armelymatona@gmail.com>
2026-06-29 13:24:34 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,16 @@
 import os
 
 # ========== CONFIGURATION BOT ==========
-# Mets ton token ici (via variable d'environnement de préférence)
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8708628597:AAEVVIO6c5wAwMRAzCl_PGttklI5lrZlV1Q")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-# ========== LISTE DES ADMINS (IDs Telegram) ==========
+# ========== LISTE DES ADMINS ==========
 AUTHORIZED_USERS = [
     8045306923, 8161643057, 7842763694, 7471493416, 8008720027, 6436665202
 ]
 
 # ========== CONFIGURATION TELEGRAM API ==========
-API_ID = int(os.environ.get("API_ID", "30062455"))       # ← Ton API_ID
-API_HASH = os.environ.get("API_HASH", "0745b6be4969fa770f3ca5493c8e797c") # ← Ton API_HASH
+API_ID = int(os.environ.get("API_ID", "0"))
+API_HASH = os.environ.get("API_HASH", "")
 
 # Session string du compte admin principal (optionnel)
 SESSION_STRING = os.environ.get("SESSION_STRING", "")
