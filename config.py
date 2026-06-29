3872451e7dc054896f3454189de29f40b7137203
armelymatona-cloud <armelymatona@gmail.com>
2026-06-29 15:20:06 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,12 @@
 import os
 
-# ========== CONFIGURATION BOT ==========
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-
-# ========== LISTE DES ADMINS ==========
 AUTHORIZED_USERS = [
     8045306923, 8161643057, 7842763694, 7471493416, 8008720027, 6436665202
 ]
-
-# ========== CONFIGURATION TELEGRAM API ==========
 API_ID = int(os.environ.get("API_ID", "0"))
 API_HASH = os.environ.get("API_HASH", "")
-
-# Session string du compte admin principal (optionnel)
 SESSION_STRING = os.environ.get("SESSION_STRING", "")
-
-# ========== CHEMINS ==========
 DATABASE_PATH = "regrand.db"
 SESSIONS_DIR = "sessions"
 ACCOUNTS_FILE = "reporting_accounts.json"
