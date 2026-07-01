7dd5dfbaaec0d1c28dfe3b091d2a35362480692f
armelymatona-cloud <armelymatona@gmail.com>
2026-07-01 15:01:43 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -482,7 +482,7 @@ def main():
     app.add_handler(CommandHandler("reconnect", reconnect))
 
     logger.info("🔄 Polling...")
-    app.run_polling(allowed_updates=Update.ALL_TYPES)
+    app.run_polling(allowed_updates=Update.ALL_TYPES, drop_pending_updates=True)
 
 
 if __name__ == "__main__":
