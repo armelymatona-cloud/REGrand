ef9bb5df99836afa4419526eb45e910d73e025fc
armelymatona-cloud <armelymatona@gmail.com>
2026-07-07 13:35:50 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,16 @@
 import os
-from telethon import TelegramClient
-from telethon.sessions import StringSession
 
-SESSION_STRING = "1BJWap1wBuwkeR-K27u6AleUDiQnL87jV70campTgUfRL7zi9k-YKvR_b03AfWai22IlRIGX2ajoRXYw5T8q8PtHK62wY_s_i8XbZhAkC-BLfb3XmoIX-PW31e6GQ-ROfKhSGbMK755ZsH78RrQkUxfDgFU1lgjYpBo2BfKfF7ArmYbIJhMe0Eyg9BaLxew1e-Kn2qsGaF94ZiVoxdAHMJW3GEBAcYiXMQG3-fl2zZbvw26qbZCTDd9cXf5YEcFOUvguJz4dg2RmqcxSXlt80r25YzwplK62c6leKZrTeIT-_o1M4YKNh8m3XLAL6PViR6TPyCf6ujOeGr3u6Qln1MverhwB4sdM="
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8175803085:AAEk0r0cueL9ub39zfBxl-GqSU91a5_snNo")
 
-API_ID = 30062455
-API_HASH = "0745b6be4969fa770f3ca5493c8e797c"
-
-# Token du bot Telegram
-BOT_TOKEN = "8175803085:AAEk0r0cueL9ub39zfBxl-GqSU91a5_snNo"
-
-# IDs Telegram des administrateurs autorisés
 AUTHORIZED_USERS = [
     8045306923, 8161643057, 7842763694, 7471493416, 8008720027, 6436665202
 ]
 
-# Chemins
+API_ID = int(os.getenv("API_ID", 30062455))
+API_HASH = os.getenv("API_HASH", "0745b6be4969fa770f3ca5493c8e797c")
+
+SESSION_STRING = os.getenv("SESSION_STRING", "")
+
 DATABASE_PATH = "regrand.db"
 SESSIONS_DIR = "sessions"
 ACCOUNTS_FILE = "reporting_accounts.json"
